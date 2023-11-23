68cc32baa326d898ad38975a953e735a5e1189aa
Sophie Delice <delicesophie824@gmail.com>
2023-11-23 10:06:18 -0500

    wip:Basic Mathematical Operations

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+
+
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - 
+// operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+function basicOp(operation, value1, value2)
+{
+    if (operation === "+"){
+        return value1 + value2;
+    }
+
+    if (operation === "-"){
+        return value1 - value2;
+    }
+    
+    if (operation === "*"){
+        return value1 * value2;
+    }
+
+    if (operation === "/"){
+        return value1 / value2;
+    }
+}
+console.log(basicOp("+ 5,5"));
+
+
+//  Write a function named setAlarm/set_alarm/set-alarm/setalarm 
+// (depending on language) which receives two parameters. 
+// The first parameter, employed,is true whenever you are employed and the second parameter,
+//  vacation is true whenever you are on vacation.
+
+function setAlarm(employed, vacation) {
+    if (employed && ! vacation ) return true; 
+  return false;
+}
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
 // You can ignore any need to sanitize words or add punctuation, 
 // but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
@@ -6,8 +42,6 @@ function smash(words) {
         return words.join(" ")
      };
 }
-
-
 // Write a function which converts the input string to uppercase.
 function makeUpperCase(str) {
     return str.toUpperCase();
