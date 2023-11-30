b4b7e03675cd204bbbf7f9e31a5b0528b439062e
Sophie Delice <delicesophie824@gmail.com>
2023-11-29 21:10:53 -0500

    wip: convert boolean values to strings yes or no

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 
+// Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+    if (bool) return "Yes"; 
+    return " No";
+}
+console.log(boolToWord("false"));
+console.log(boolToWord("true"));
+
+
 // Remove String Spaces
 //  Write a function that removes the spaces from the string, then return the resultant string.
 
