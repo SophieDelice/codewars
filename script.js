d40fdc3ebba7771f9408f720ccf9a16b438d3b1b
Sophie Delice <delicesophie824@gmail.com>
2023-11-30 23:28:41 -0500

    wip:calculate BMI

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
+// Calculate BMI
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  const bmiTotal = weight / (height **2 ); 
+  if ( bmiTotal <= 18.5)  return "Underweight";
+  if (bmiTotal<= 25.0 ) return "Normal"; 
+  if (bmiTotal<= 30.0) return "Overweight"; 
+  if (bmiTotal > 30 ) return "Obese";
+  
+}
+
+
+console.log(bmi( 375, 5.8));
+
 
 // Convert boolean values to strings 'Yes' or 'No'.
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
