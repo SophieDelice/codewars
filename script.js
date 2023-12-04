e17b24147d055e438d109ab5a8b7ccadd7ed2e96
Sophie Delice <delicesophie824@gmail.com>
2023-12-04 18:27:32 -0500

    wip: Abbreviate a Two Word Name

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
 
+// Abbreviate a Two Word Name
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+
+function abbrevName(name){
+  let [ first , last] = name.split(' '); 
+  return first [0].toUpperCase() + '.' + last[0].toUpperCase(); 
+
+    // code away
+
+}
+
+
 // Double Char
 // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
