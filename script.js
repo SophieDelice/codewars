74a685cd660c456765c601053c6b817cb16efdcc
Sophie Delice <delicesophie824@gmail.com>
2023-12-11 21:50:00 -0500

    wip:The highest profit wins

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
 
+// The highest profit wins!
+// Story - Ben has a very simple idea to make some profit: he buys something and sells it again. Of course,
+//   this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, 
+//  he's going to buy it for the lowest possible price and sell it at the highest.
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)]; // fix me!
+}
+arr = [ 2,9,7,24]
+console.log(minMax(arr))
+
+
 // Opposites Attract
 // Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
 // If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
