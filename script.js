0bb22c60de949bf5034dfb424d104c630a0671d8
Sophie Delice <delicesophie824@gmail.com>
2023-12-11 21:29:00 -0500

    wip:Opposites Attract

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 
 
+// Opposites Attract
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+// If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  
+  if (flower1 % 2 === 0 && flower2 % 2 === 1) return true
+  if (flower1 % 2 === 1 && flower2 % 2 === 0 ) return true
+  return false ;
+}
+
+console.log(lovefunc(10, 3) ); 
+console.log(lovefunc(5, 3)); 
+
+
 // Are You Playing Banjo?
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo! The function takes a name as its only argument, 
