9bb95040a8b3f92917519f324b2b8b701c505519
Sophie Delice <delicesophie824@gmail.com>
2023-12-14 20:09:15 -0500

    wip:simple multiplication

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
+// Simple multiplication
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+  // your code........
+if (number % 2 === 0) return number * 8; 
+return number * 9; 
+}
 
 // Area or Perimeter
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
