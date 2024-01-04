c7465edf533aa57f30aa3cc39ab8baeb54707976
Sophie Delice <delicesophie824@gmail.com>
2024-01-03 23:01:45 -0500

    wip:Grasshopper - Personalized Message

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,5 +1,19 @@
 
 
+// Grasshopper - Personalized Message
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message:
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+
+function greet (name, owner) {
+  // Add code here
+  if( name === owner ) return "Hello boss";
+  else return "Hello guest"
+}
+
 // Calculate Average 
 // Write a function which calculates the average of the numbers in a given list.
 function findAverage(array) {
