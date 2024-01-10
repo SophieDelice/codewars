c41e783ffd072bf173572b52eaebc435fa7d02cc
Sophie Delice <delicesophie824@gmail.com>
2024-01-09 19:25:18 -0500

    wip:Beginner - Lost Without a Map

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,5 +1,17 @@
 
 
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x){
+  let result = []; 
+  for ( let i=0; i<x.length; i++){
+    result.push(x[i] *2); 
+  }
+  return result 
+}
+
 // Powers of 2
 // Complete the function that takes a non-negative integer n as input, and returns a list of all the 
 // powers of 2 with the exponent ranging from 0 to n ( inclusive ).
