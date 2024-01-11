4578a4722fd08428b8bb11dc0e2a64f03f66f009
Sophie Delice <delicesophie824@gmail.com>
2024-01-10 21:35:49 -0500

    wip:Array.diff

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,4 +1,14 @@
 
+// Array.diff
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  return a.filter(number => !b.includes(number));
+}
 
 //Is he gonna survive?
 // A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons!
