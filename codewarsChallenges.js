1e7b65a2c40ce6273ebada431ac6fc40e5b84424
Sophie Delice <delicesophie824@gmail.com>
2024-01-30 20:32:18 -0500

    wip:Function 2 - squaring an argument

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,4 +1,10 @@
 
+// Function 2 - squaring an argument
+// Now you have to write a function that takes an argument and returns the square of it.
+// Write the "square"-function here
+const square = function(n) {
+  return n*n;
+}
 
 // Grasshopper - Basic Function Fixer
 // Fix the function
