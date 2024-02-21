5851fafb4939c1ba89ab14cd55ae5f5b83aa8c29
Sophie Delice <delicesophie824@gmail.com>
2024-02-20 23:14:06 -0500

    wip: School paperwork

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,4 +1,16 @@
 
+
+
+// Beginner Series #1 School Paperwork
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+if (n < 0 || m < 0) {
+  return 0;
+} else {
+  return n * m;
+}
+
 // Switch it Up!
 // When provided with a number between 0-9, return it in words.
 // Input :: 1
