362165c967baa69c91fbca59f5e23e146fbbd326
Sophie Delice <delicesophie824@gmail.com>
2024-03-01 19:31:56 -0500

    wip:Remove anchor from URL

--- codewarsChallenges.js
+++ codewarsChallenges.js
@@ -1,4 +1,15 @@
 
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+  // TODO: complete
+  return url.split("#")[0];
+}
+
 // Difference of Volumes of Cuboids
 
 // In this simple exercise, you will create a program that will take two lists of integers, a and b.
